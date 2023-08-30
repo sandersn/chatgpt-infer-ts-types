27c417f33692ba928a5e8ce8e7f32522a63730c1
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-08-30 09:50:58 -0700

    Add stubs for examples + extract function test

--- example3.ts
+++ example3.ts
@@ -37,3 +37,81 @@ class C implements Person {
     this.name = name;
   }
 }
+function bifilter<T>(xs: T[], predicate: (t: T) => boolean): T[][] {
+  let yeses = []
+  let nos = []
+  for (const x of xs) {
+    const newLocal = predicate(x);
+    if (newLocal) {
+      yeses.push(x)
+    } else {
+      nos.push(x)
+    }
+  }
+  return [yeses, nos]
+}
+
+interface Channel {
+  id: number
+  name: string
+  url: URL
+  subscribers: Subscriber[]
+}
+interface Subscriber {
+  id: number
+  name: string
+  email: string
+  follows: Channel[]
+}
+/** this is a test of the extract function refactor 
+ * 
+ * Finds the subscriber with the earliest id.
+ * Then it transforms that to a formatted list.
+ */
+function showEarliestSubscriber(subs: Subscriber[]) {
+  let earliest = subs[0]
+  let earliestId = Number.MAX_SAFE_INTEGER
+  for (const sub of subs) {
+    if (sub.id < earliestId) {
+      earliest = sub
+      earliestId = sub.id
+    }
+  }
+  let formatted = `${earliest.name} <${earliest.email}>`
+  for (const follow of earliest.follows) {
+    formatted += `\n- ${follow.name} (${follow.url})`
+  }
+  return formatted
+}
+function extractLocal() {
+
+}
+class ExtractLocal() {
+  extractLocal() {
+
+  }
+}
+function extractTypeAlias() {
+
+}
+function extractInterface() {
+
+}
+function nameInferredReturnType() {
+
+}
+type NameDefaultedParameters = (string, string) => [string, string]
+implementMissingFunction(extractFunction, 101)
+
+abstract class Base {
+
+}
+interface Specification {
+
+}
+class ImplementAbstractMethod extends Base {
+
+}
+class ImplementSpecification implements Specification {
+
+}
